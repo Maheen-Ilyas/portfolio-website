6f702d8a414fff733d3b3d2797bbba650553a635
Maheen Ilyas <mahilyas05@gmail.com>
2024-09-09 16:14:13 +0300

    Changed the shape of 3D objects

--- src/components/canvas/Orb.jsx
+++ src/components/canvas/Orb.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Box, Sphere, Cone } from "@react-three/drei";
+import { OrbitControls, Box } from "@react-three/drei";
 
 const OrbCanvas = () => {
   const [shapeSize, setShapeSize] = useState(1);
   const [positions, setPositions] = useState({
-    box: [-3, 1.4, 1],
-    sphere: [5, 0, 0.4],
-    cone: [-3, -1.6, 0],
+    box1: [-3, 1.4, 1],
+    box2: [5, 0, 0.4],
+    box3: [-3, -1.6, 0],
   });
 
   useEffect(() => {
@@ -15,16 +15,16 @@ const OrbCanvas = () => {
       if (window.innerWidth <= 500) {
         setShapeSize(0.6);
         setPositions({
-          box: [-1, 0.8, 0.5],
-          sphere: [1, 0, 0.2],
-          cone: [-1, -0.8, 0],
+          box1: [-1, 0.8, 0.5],
+          box2: [1, 0, 0.2],
+          box3: [-1, -0.8, 0],
         });
       } else {
         setShapeSize(1);
         setPositions({
-          box: [-3, 1.4, 1],
-          sphere: [5, 0, 0.4],
-          cone: [-3, -1.6, 0],
+          box1: [-3, 1.4, 1],
+          box2: [5, 0, 0.4],
+          box3: [-3, -1.6, 0],
         });
       }
     };
@@ -44,21 +44,22 @@ const OrbCanvas = () => {
       }}
       camera={{ position: [0, 0, 5], fov: 75 }}
     >
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 10, 5]} intensity={0.5} />
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[10, 10, 5]} intensity={1} />
+      <pointLight position={[-10, -10, 10]} intensity={0.7} />
       <OrbitControls enableZoom={false} />
-      <Box position={positions.box} scale={[shapeSize, shapeSize, shapeSize]}>
-        <meshStandardMaterial color="#3c444C" />
+      <Box position={positions.box1} scale={[shapeSize, shapeSize, shapeSize]}>
+        <meshStandardMaterial color="#444A5B" />
       </Box>
-      <Sphere
-        position={positions.sphere}
+      <Box
+        position={positions.box2}
         scale={[shapeSize, shapeSize, shapeSize]}
       >
-        <meshStandardMaterial color="#3c444C" />
-      </Sphere>
-      <Cone position={positions.cone} scale={[shapeSize, shapeSize, shapeSize]}>
-        <meshStandardMaterial color="#3c444C" />
-      </Cone>
+        <meshStandardMaterial color="#444A5B" />
+      </Box>
+      <Box position={positions.box3} scale={[shapeSize, shapeSize, shapeSize]}>
+        <meshStandardMaterial color="#444A5B" />
+      </Box>
     </Canvas>
   );
 };
