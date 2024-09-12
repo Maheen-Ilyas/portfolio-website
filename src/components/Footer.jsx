db2dc96336620e3d1a3f1569696d997293185ab0
Maheen Ilyas <mahilyas05@gmail.com>
2024-09-12 17:19:11 +0300

    Changed the quote

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,8 @@ const Footer = () => {
     <footer className="w-full bg-[#12171D] py-6">
       <div className="max-w-7xl mx-auto px-4 flex flex-col items-center justify-center text-center space-y-4">
         <p className="text-white text-[16px] font-playfair italic">
-          "The only way to do great work is to love what you do." – Steve Jobs
+          "The answer to the ultimate question of life, the universe, and
+          everything is 42."
         </p>
         <p className="text-white text-[14px] font-poppins">
           © {new Date().getFullYear()} Maheen Ilyas. All rights reserved.
